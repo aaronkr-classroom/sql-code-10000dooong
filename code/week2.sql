800ffecb7ef02c5e742ae8269b8e03e35c52217f
10000dooong <162276811+10000dooong@users.noreply.github.com>
2024-09-10 12:36:46 +0900

    week2 제출

---
+++ code/week2.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE dju24;
+
+CREATE TABLE teacher (
+	id bigserial,
+	first_name varchar(25),
+	last_name varchar(50),
+	school varchar(50),
+	hire_date date,
+	salary numeric
+);
+
+INSERT INTO teacher (first_name, last_name, school, hire_date, salary)
+VALUES ('Janet', 'Smith', 'F.D.Roosevelt H.S.', '2011-10-30', 36200),
+		('Lee', 'Reynolds', 'F.D.Roosevelt H.S.', '1993-05-22', 65000),
+		('Samuel', 'Cole', 'Myers Middle School', '2005-08-01', 43500);
+
+
+SELECT * from teacher;
+
+
+
+
+
+
+
+
+
+
+
+
+
